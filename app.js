28349dc968800958c34efc27f09fe09cec103847
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-11 21:29:41 +1000

    img

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var app = express();   //#1app has the all the methods of express able to be acc
 
 app.set('view engine', 'ejs');
 
+// #add after moved routes
+var routes = require('./routes');
+//#specify the directory of static assets, do after adding each method in the view
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));        //?????????????? #static assets are path in public path directory
+                                                                 //make app automatically look in the public directory for static assets(everything in the pub directory like images, stylesheets)
+
 
 
 //#sepecify routes
@@ -12,25 +19,30 @@ app.set('view engine', 'ejs');
 //  res.send("<h2>This is a server response on the home page</h2>")
 // });
 
-//#6 home routes #after creating views folder and home.ejs
-app.get('/', function(req, res){
- res.render('../views/home', {
-   title: "Star Wars Movies",
-   movies : ["The First Movie", "The second Movie", "The third Movie"] //#getting the arrays form the later json file 
- });
-});
-
-//movie_single //#4
-app.get('/star_wars_episode/:episode_number?', function(req, res){
- var episode_number = req.params.episode_number;      //#store own var called epidsode_number #to get the epidose number
-     res.send("This is the page for episode " + episode_number);
-});
-
-//not found //#5
-app.get('*', function(req, res){   //# "*" represents any route
- res.send("This is not the page that you are looking for")
-});
+// //#6 home routes #after creating views folder and home.ejs
+// app.get('/', function(req, res){
+//  res.render('../views/home', {
+//    title: "Star Wars Movies"//, comment out when deletebelow movies
+//   //  movies : ["The First Movie", "The second Movie", "The third Movie"] //#getting the arrays form the later json file #comment out after adding bootstrap in home.ejs
+//  });
+// });
 //
+// //movie_single //#4
+// app.get('/star_wars_episode/:episode_number?', function(req, res){
+//  var episode_number = req.params.episode_number;      //#store own var called epidsode_number #to get the epidose number
+//      res.send("This is the page for episode " + episode_number);
+// });
+//
+// //not found //#5
+// app.get('*', function(req, res){   //# "*" represents any route
+//  res.send("This is not the page that you are looking for")
+// });
+// //
+
+// #after require routes, change above like below, factoring and comment out above
+app.get('/', routes.home);
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+app.get('*', routes.notFound);
 
 
 //server calling //#3
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+//#after creating movies.json and store data, do following
+//#creating routes folder after creating public and copy&paste #4-6 in app.js to setup routes
+//#and change from app.get to exports
+
+//#after copy images from github, create var moviesJSON so that access to the images
+var moviesJSON = require('../movies.json');
+
+//#6
+exports.home = function(req, res){
+  var Movies = moviesJSON.movies;  //#to get movies array
+  res.render('../views/home', {
+   title : "Star Wars Movies",
+   movies : Movies}); //#pass Movies
+};
+
+//#4
+exports.movie_single = function(req, res){
+ var episode_number = req.params.episode_number;
+     res.send("This is the page for episode " + episode_number);
+};
+
+//#5
+exports.notFound = function(req, res){
+ res.send("This is not the page that you are looking for")
+};
